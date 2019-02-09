4639dc5d2117aa588e125b5a1c3ea65f92c420ff
Ugo Sangiorgi <ugo.sangiorgi@elastic.co>
2019-02-09 16:17:29 -0500

    Create generate_load.py

---
+++ pycon2019-app/myproject/myproject/management/commands/generate_load.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand, CommandError
+from myproject.models import Actor, Movie, Role
+import tarfile
+import os
+import json
+import requests
+import time
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        while(True):
+            random_actor = Actor.objects.order_by('?').first()
+            requests.get('http://localhost:8000?search_box'%random_actor.name)
+            time.sleep(1)
